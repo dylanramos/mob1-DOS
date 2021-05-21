7c5d17496eb26f77cd0eb2ccc84dd007eb3cd462
OLIVEIRA-RAMOS Dylan <Dylan.OLIVEIRA-RAMOS@cpnv.ch>
2021-05-21 12:17:57 +0200

    Display the login page

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, View,} from 'react-native';
+import LoginForm from "./components/LoginForm";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto"/>
+            <LoginForm/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        maxWidth: 480,
+        backgroundColor: "#f0f2f5",
+        padding: 20,
+        justifyContent: "center",
+        alignItems: "center",
+    },
 });
