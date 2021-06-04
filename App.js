74769d5527d997bc9f747b6f5b2bb3c0d8002fb0
OLIVEIRA-RAMOS Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-06-04 09:59:58 +0200

    Navigate to the home page after successful authentication

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
@@ -7,19 +7,47 @@ import HomeScreen from "./screens/HomeScreen";
 
 const Stack = createStackNavigator();
 
-export default function App() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator screenOptions={{headerShown: false}}>
-                <Stack.Screen
-                    name="Login"
-                    component={LoginScreen}
-                />
-                <Stack.Screen
-                    name="Home"
-                    component={HomeScreen}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            user: null,
+            base: null,
+            authToken: null,
+        }
+    }
+
+    /**
+     * Stores the user authentication data
+     * @param user
+     * @param base
+     * @param authToken
+     */
+    authenticate = (user, base, authToken) => {
+        console.log(user, base, authToken)
+        this.setState({
+            user: user,
+            base: base,
+            authToken: authToken,
+        })
+    }
+
+    render() {
+        return (
+            <NavigationContainer>
+                <Stack.Navigator screenOptions={{headerShown: false}}>
+                    {this.state.authToken == null ? (
+                        <Stack.Screen name="Login">{() => <LoginScreen
+                            authenticate={this.authenticate}/>}</Stack.Screen>
+                    ) : (
+                        <Stack.Screen name="Home">
+                            {() => <HomeScreen user={this.state.user} base={this.state.base}
+                                               authToken={this.state.authToken}/>}
+                        </Stack.Screen>
+                    )}
+                </Stack.Navigator>
+            </NavigationContainer>
+        );
+    }
 }
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,13 +1,109 @@
-import React from "react";
-import {View, StyleSheet} from "react-native";
-import LoginForm from "../components/LoginForm";
+import React from 'react';
+import {Button, Text, TextInput, View, Picker, StyleSheet} from "react-native";
+import axios from "axios";
+import ServerUrl from "../ServerUrl"
+import ErrorMessage from "../components/ErrorMessage";
 
-export default function LoginScreen({navigation}) {
-    return (
-        <View style={styles.container}>
-            <LoginForm navigation={navigation}/>
-        </View>
-    )
+export default class LoginScreen extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            bases: [],
+            buttonDisabled: true,
+            initials: null,
+            password: null,
+            base: null,
+            showErrorMessage: false,
+        };
+    }
+
+    /**
+     * Load the bases using the api
+     */
+    componentDidMount() {
+        axios.get(`${ServerUrl}bases`)
+            .then(res => {
+                this.setState({bases: res.data})
+
+                //Set the first base as default
+                this.handlePickerChange(this.state.bases[0].name)
+            })
+    }
+
+    /**
+     * Update the state of the text input and the submit button
+     * @param input
+     * @param value
+     */
+    handleTextChange(input, value) {
+        this.setState(
+            {
+                [input]: value,
+            },
+            this.changeButtonState
+        );
+    }
+
+    /**
+     * Update the state of the picker
+     * @param value
+     */
+    handlePickerChange = (value) => {
+        this.setState(
+            {
+                base: value
+            }
+        )
+    }
+
+    /**
+     * Enable or disable the submit button
+     */
+    changeButtonState = () => {
+        if (this.state.initials !== "" && this.state.password !== "")
+            this.setState({buttonDisabled: false})
+        else
+            this.setState({buttonDisabled: true})
+    }
+
+    /**
+     * Authenticate or not after submitting form
+     */
+    submitForm = () => {
+        axios.post(`${ServerUrl}gettoken`, {
+            initials: this.state.initials,
+            password: this.state.password,
+        })
+            .then((response) => {
+                this.setState({showErrorMessage: false})
+                this.props.authenticate(this.state.initials, this.state.base, response.data.token)
+            })
+            .catch(() => {
+                this.setState({showErrorMessage: true})
+            })
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.form}>
+                    <Text style={styles.title}>Connexion</Text>
+                    {this.state.showErrorMessage ?
+                        <ErrorMessage message={"Les données entrées sont incorrectes"}/> : null}
+                    <TextInput style={styles.input} placeholder="Initiales" autoFocus={true}
+                               onChangeText={(value) => this.handleTextChange("initials", value)}/>
+                    <TextInput style={styles.input} placeholder="Mot de passe" secureTextEntry={true}
+                               onChangeText={(value) => this.handleTextChange("password", value)}/>
+                    <Picker style={styles.picker} onValueChange={this.handlePickerChange}>
+                        {this.state.bases.map(base => <Picker.Item key={base.id} label={base.name} value={base.name}/>)}
+                    </Picker>
+                    <Button title="Connexion" color="#326fa8" disabled={this.state.buttonDisabled}
+                            onPress={this.submitForm}/>
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
@@ -19,4 +115,25 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
     },
+    form: {
+        width: "75%",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: "bold",
+        marginBottom: 20,
+    },
+    input: {
+        height: 40,
+        width: "100%",
+        borderBottomWidth: 1,
+        borderBottomColor: "black",
+        marginBottom: 20,
+    },
+    picker: {
+        width: "100%",
+        marginBottom: 20,
+    }
 });
