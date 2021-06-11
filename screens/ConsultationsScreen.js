2342f42c52acb35bc733f486b2fa719f114187a4
OLIVEIRA-RAMOS Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-06-11 08:37:51 +0200

    Display the reports

--- screens/ConsultationsScreen.js
+++ screens/ConsultationsScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {View, Text, FlatList} from "react-native";
+import {View, Text, FlatList, Button, StyleSheet} from "react-native";
 import axios from "axios";
 import ServerUrl from "../ServerUrl";
 
@@ -10,6 +10,7 @@ export default class ConsultationsScreen extends React.Component {
         this.state = {
             drugs: [],
             shifts: [],
+            drugsButtonDisabled: true,
         }
     }
 
@@ -29,20 +30,58 @@ export default class ConsultationsScreen extends React.Component {
         })
     }
 
+    /**
+     * Change the button state to display the corresponding data
+     */
+    handleTabChange = () => {
+        if (this.state.drugsButtonDisabled)
+            this.setState({
+                drugsButtonDisabled: false,
+            })
+        else
+            this.setState({
+                drugsButtonDisabled: true,
+            })
+    }
+
     render() {
         return (
-            <View>
-                <FlatList
-                    data={this.state.drugs}
-                    renderItem={({item}) => (<Text>Le {item.date} à {item.base}</Text>)}
-                    keyExtractor={item => item.id.toString()}
-                />
-                <FlatList
-                    data={this.state.shifts}
-                    renderItem={({item}) => (<Text>Le {item.date} à {item.base}</Text>)}
-                    keyExtractor={item => item.id.toString()}
-                />
+            <View style={styles.container}>
+                <View style={styles.buttonsContainer}>
+                    <Button title="Stup" color="#326fa8" disabled={this.state.drugsButtonDisabled}
+                            onPress={this.handleTabChange}/>
+                    <Button title="Garde" color="#326fa8" disabled={!this.state.drugsButtonDisabled}
+                            onPress={this.handleTabChange}/>
+                </View>
+                {this.state.drugsButtonDisabled ? (
+                    <FlatList
+                        data={this.state.drugs}
+                        renderItem={({item}) => (<Text>Le {item.date} à {item.base}</Text>)}
+                        keyExtractor={item => item.id.toString()}
+                    />
+                ) : (
+                    <FlatList
+                        data={this.state.shifts}
+                        renderItem={({item}) => (<Text>Le {item.date} à {item.base}</Text>)}
+                        keyExtractor={item => item.id.toString()}
+                    />
+                )}
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        maxWidth: 480,
+        backgroundColor: "#f0f2f5",
+        padding: 20,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    buttonsContainer: {
+        maxWidth: 480,
+        flexDirection: "row",
+    },
+});
