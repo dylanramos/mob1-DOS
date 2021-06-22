26d1f602347c2ba3d0eb7ca765243d216b5da17e
OLIVEIRA-RAMOS Dylan <Dylan.OLIVEIRA-RAMOS@cpnv.ch>
2021-06-22 10:11:38 +0200

    Have the work times to confirm button

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,41 @@
 import React from "react";
 import {View, Button, StyleSheet} from "react-native";
+import axios from "axios";
+import ServerUrl from "../ServerUrl";
 
 export default class HomeScreen extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            showWorkTimesButton: true,
+            workTimesNumberToConfirm: 0
+        }
+    }
+
+    /**
+     * Make an api call to get all the unconfirmed work plans
+     */
+    componentDidMount() {
+        axios.get(`${ServerUrl}unconfirmedworkplans`, {
+            headers: {
+                'Authorization': `Bearer ${this.props.authToken}`
+            }
+        }).then(res => {
+            // Hide button when there is no data
+            if (res.data < 1)
+                this.setState({
+                    showWorkTimesButton: false,
+                })
+
+            this.setState({
+                workTimesNumberToConfirm: res.data.length
+            })
+        }).catch(() => {
+            this.setState({
+                showWorkTimesButton: false,
+            })
+        })
     }
 
     render() {
@@ -18,6 +50,12 @@ export default class HomeScreen extends React.Component {
                         <Button title="Rapporter" color="#326fa8"
                                 onPress={() => this.props.navigation.navigate("Report")}/>
                     </View>
+                    {this.state.showWorkTimesButton ? (
+                        <View style={styles.buttonContainer}>
+                            <Button title={`${this.state.workTimesNumberToConfirm} Horaires à confirmer`} color="#326fa8"
+                                    onPress={() => this.props.navigation.navigate("WorkTimesToConfirm")}/>
+                        </View>
+                    ) : null}
                     <View style={styles.buttonContainer}>
                         <Button title="Déconnexion" color="#326fa8"
                                 onPress={() => this.props.navigation.navigate("Logout")}/>
