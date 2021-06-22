f803bde03c657f420e9ba7529877fb8377565a29
OLIVEIRA-RAMOS Dylan <Dylan.OLIVEIRA-RAMOS@cpnv.ch>
2021-06-22 11:49:50 +0200

    Be able to submit the work time

--- components/WorkTimeToConfirmForm.js
+++ components/WorkTimeToConfirmForm.js
@@ -2,6 +2,7 @@ import React from "react";
 import {View, Text, StyleSheet, TextInput, Button, Picker} from "react-native";
 import axios from "axios";
 import ServerUrl from "../ServerUrl";
+import ErrorMessage from "./ErrorMessage";
 
 export default class WorkTimeToConfirmForm extends React.Component {
     constructor(props) {
@@ -11,6 +12,7 @@ export default class WorkTimeToConfirmForm extends React.Component {
             workTimeStatus: [{id: 0, name: "A discuter"}, {id: 1, name: "Confirmé"}],
             currentStatus: this.props.workTime.confirmation,
             reason: this.props.workTime.reason,
+            showErrorMessage: false,
         }
     }
 
@@ -36,6 +38,22 @@ export default class WorkTimeToConfirmForm extends React.Component {
         })
     }
 
+    submitForm = () => {
+        axios.post(`${ServerUrl}confirmworkplan`, {
+            id: this.props.workTime.id,
+            confirmation: this.state.currentStatus,
+            reason: this.state.reason,
+        }, {
+            headers: {
+                'Authorization': `Bearer ${this.props.authToken}`
+            }
+        }).catch(() => {
+            this.setState({
+                showErrorMessage: true,
+            })
+        })
+    }
+
     render() {
         return (
             <View style={styles.container}>
@@ -50,6 +68,9 @@ export default class WorkTimeToConfirmForm extends React.Component {
                                                                                       label={status.name}
                                                                                       value={status.id}/>)}
                             </Picker>
+                            {this.state.showErrorMessage ? (
+                                <ErrorMessage message={"Votre raison est trop courte."}/>
+                            ) : null}
                             <Text>Raison: </Text><TextInput style={styles.textInput}
                                                             value={this.state.reason != null ? this.state.reason : ""}
                                                             onChangeText={this.handleTextInputChange}/>
